46e99d31f3e0ddf417e032a8a3900e7e336738b8
jg <57115019+joeyagreco@users.noreply.github.com>
2023-11-02 00:19:28 -0500

    stuff

--- main.bat
+++ main.bat
@@ -6,6 +6,12 @@ IF "%1"=="fmt" (
 ) ELSE IF "%1"=="up-reqs" (
     REM update requirements.txt file automatically
     call :run_up_reqs
+) ELSE IF "%1"=="docker-build" (
+    REM build docker image
+    call :run_docker_build
+) ELSE IF "%1"=="docker" (
+    REM run docker image
+    call :run_docker
 ) ELSE (
     echo Invalid flag. Please specify a valid flag.
 )
@@ -19,4 +25,12 @@ exit /b
 
 :run_up_reqs
 pipreqs . --force
+exit /b
+
+:run_docker_build
+docker build -t daily_chess .
+exit /b
+
+:run_docker
+docker run daily_chess
 exit /b
