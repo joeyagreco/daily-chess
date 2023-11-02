601020ada67c2a4d39aa4126a3254895bdd429ec
jg <57115019+joeyagreco@users.noreply.github.com>
2023-11-02 00:09:52 -0500

    stuff

--- main.bat
+++ main.bat
@@ -3,6 +3,9 @@
 IF "%1"=="fmt" (
     REM format code
     call :run_fmt
+) ELSE IF "%1"=="up-reqs" (
+    REM update requirements.txt file automatically
+    call :run_up_reqs
 ) ELSE (
     echo Invalid flag. Please specify a valid flag.
 )
@@ -12,4 +15,8 @@ black --config=pyproject.toml .
 autoflake --config=pyproject.toml .
 REM isort automatically looks for pyproject.toml
 isort .
+exit /b
+
+:run_up_reqs
+pipreqs . --force
 exit /b
