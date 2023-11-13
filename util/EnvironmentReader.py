e16cbb5d984f3d60bb0cd521d341ddee65e43311
jg <57115019+joeyagreco@users.noreply.github.com>
2023-11-13 09:46:56 -0600

    bug fix

--- util/EnvironmentReader.py
+++ util/EnvironmentReader.py
@@ -1,10 +1,14 @@
 import os
+from typing import Optional
 
 from dotenv import load_dotenv
 
 
 class EnvironmentReader:
     @staticmethod
-    def get(name: str) -> str:
+    def get(name: str, default: Optional[any] = None) -> str:
         load_dotenv()
-        return os.getenv(name)
+        env_var = os.getenv(name)
+        if env_var is None:
+            env_var = default
+        return env_var
